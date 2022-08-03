a7edfd42c15a64233f1a74c19c7df61535421756
ericc70 <eric070@net-c.fr>
2022-08-03 19:58:29 +0200

    add materialize , path,

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const path = require('path');
 
 const connectDB = require("./config/db")
+const { patch } = require("./routes")
 
 // Load config
 dotenv.config({ path: "./config/config.env" })
@@ -20,6 +22,9 @@ if(process.env.NODE_ENV === 'developpement')
 //handlebar
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
