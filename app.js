fb9a079b2f5f92409ba0db54c6371493b7dc9c3a
ericc70 <eric070@net-c.fr>
2022-08-09 13:11:15 +0200

    add update stori methode

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require('method-override');
 const path = require("path");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
@@ -19,6 +20,18 @@ require("./config/passport")(passport);
 connectDB();
 const app = express();
 
+
+// Method override
+app.use(
+  methodOverride( (req, res) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
 //body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
